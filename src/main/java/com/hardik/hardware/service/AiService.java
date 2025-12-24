a6dc9906d9094ceba3b9fecb8da8b5e8b5b03e13
Hardik-Kumar0005 <hardikkumarpro0005@gmail.com>
2025-12-25 00:35:59 +0530

    add IngestionService to feed data into the vector store

--- src/main/java/com/hardik/hardware/service/AiService.java
+++ src/main/java/com/hardik/hardware/service/AiService.java
@@ -1,6 +1,8 @@
 package com.hardik.hardware.service;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
+import org.springframework.ai.vectorstore.pgvector.PgVectorStore;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 
@@ -9,9 +11,10 @@ public class AiService {
 
     private final ChatClient chatClient;
 
-    public AiService(ChatClient.Builder chatClient) {
+    public AiService(ChatClient.Builder chatClient, PgVectorStore vectorStore) {
         this.chatClient = chatClient
-                .defaultSystem("you are an expert on hardware components required for hardware projects")
+                .defaultAdvisors(QuestionAnswerAdvisor.builder(vectorStore).build())
+                .defaultSystem("you are an expert on hardware components required for hardware projects. List out all the components necessary to build the project and the software/libraries required for programming the device and handling the sensor data.")
                 .build();
     }
 
---
+++ src/main/java/com/hardik/hardware/service/IngestionService.java
@@ -0,0 +1,40 @@
+package com.hardik.hardware.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.ai.reader.tika.TikaDocumentReader;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import org.springframework.ai.document.Document;
+
+import java.util.List;
+
+
+@Component
+public class IngestionService implements CommandLineRunner {
+
+    private static final Logger log = LoggerFactory.getLogger(IngestionService.class);
+    private final VectorStore vectorStore;
+
+    public IngestionService(VectorStore vectorStore) {
+        this.vectorStore = vectorStore;
+    }
+
+    @Override
+    public void run(String... args) {
+        List<String> projectUrls = List.of(
+                "https://www.instructables.com/Build-a-Simple-IOT-Weather-Station/",
+                "https://randomnerdtutorials.com/esp32-dht11-dht22-temperature-humidity-sensor-arduino-ide/"
+        );
+        for (String url : projectUrls) {
+            TikaDocumentReader reader = new TikaDocumentReader(url);
+            List<Document> docs = reader.get();
+            TokenTextSplitter splitter = new TokenTextSplitter();
+            vectorStore.accept(splitter.apply(docs));
+            log.info("{} has been read", url);
+        }
+    }
+
+}
